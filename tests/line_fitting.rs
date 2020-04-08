0ae3d27ce115943fe81c7e3a96c425c8503bae9f
Jannik Schürg <jannik.schuerg@posteo.de>
2020-04-08 11:46:51 +0200

    Rework API to use a optimization target trait

--- tests/line_fitting.rs
+++ tests/line_fitting.rs
@@ -1,5 +1,10 @@
 use arrsac::{Arrsac, Config};
-use nalgebra::{dimension::U2, Dynamic, Matrix, Matrix3x2, VecStorage, Vector2, Vector3};
+use levenberg_marquardt::{LeastSquaresProblem, LevenbergMarquardt};
+use nalgebra::{
+    dimension::{U1, U2, U3},
+    storage::Owned,
+    Dynamic, Matrix, Matrix3x2, VecStorage, Vector2, Vector3,
+};
 use pcg_rand::Pcg64;
 use rand::distributions::Uniform;
 use rand::{distributions::Distribution, Rng};
@@ -8,15 +13,15 @@ use sample_consensus::{Consensus, Estimator, Model};
 const RESIDUAL_SCALE: f32 = 0.1;
 const LINES_TO_ESTIMATE: usize = 1000;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Line {
-    norm: Vector2<f32>,
+    normal: Vector2<f32>,
     c: f32,
 }
 
 impl Line {
     fn xy_residuals(&self, point: Vector2<f32>) -> Vector2<f32> {
-        -self.norm * self.c - self.norm.dot(&point) * self.norm
+        (self.c - self.normal.dot(&point)) * self.normal
     }
 
     /// This takes in a point and computes the Jacobian of the vector from
@@ -31,34 +36,34 @@ impl Line {
     #[rustfmt::skip]
     fn jacobian(&self, point: Vector2<f32>) -> Matrix3x2<f32> {
         let (sx, sy) = (point.x, point.y);
-        let (nx, ny) = (self.norm.x, self.norm.y);
+        let (nx, ny) = (self.normal.x, self.normal.y);
         let c = self.c;
         Matrix3x2::new(
-            c + 2.0 * sx * nx + sy * ny,    sx * ny,
-            sy * nx,                        c + sx * nx + 2.0 * sy * ny,
+            c - 2.0 * sx * nx - sy * ny,   -sx * ny,
+            - sy * nx,                      c - sx * nx - 2.0 * sy * ny,
             nx,                             ny,
         )
     }
 
     fn into_vec(self) -> Vector3<f32> {
-        self.norm.push(self.c)
+        self.normal.push(self.c)
     }
 
     fn from_vec(v: Vector3<f32>) -> Self {
         Self {
-            norm: v.xy(),
+            normal: v.xy(),
             c: v.z,
         }
     }
 
     fn norm_cosine_distance(&self, other: &Self) -> f32 {
-        1.0 - self.norm.dot(&other.norm).abs()
+        1.0 - self.normal.dot(&other.normal).abs()
     }
 }
 
 impl Model<Vector2<f32>> for Line {
     fn residual(&self, point: &Vector2<f32>) -> f32 {
-        (self.norm.dot(point) + self.c).abs()
+        (self.normal.dot(point) - self.c).abs()
     }
 }
 
@@ -75,9 +80,44 @@ impl Estimator<Vector2<f32>> for LineEstimator {
     {
         let a = data.next().unwrap();
         let b = data.next().unwrap();
-        let norm = Vector2::new(a.y - b.y, b.x - a.x).normalize();
-        let c = -norm.dot(&b);
-        std::iter::once(Line { norm, c })
+        let normal = Vector2::new(a.y - b.y, b.x - a.x).normalize();
+        let c = -normal.dot(&b);
+        std::iter::once(Line { normal, c })
+    }
+}
+
+#[derive(Clone)]
+struct LineFittingOptimizationProblem<'a> {
+    points: &'a [Vector2<f32>],
+    model: Line,
+}
+
+impl<'a> LeastSquaresProblem<f32, U3, Dynamic, U2> for LineFittingOptimizationProblem<'a> {
+    type ParameterStorage = Owned<f32, U3, U1>;
+    type JacobianStorage = Owned<f32, U3, U2>;
+    type ResidualStorage = VecStorage<f32, U2, Dynamic>;
+
+    fn apply_parameter_step(&mut self, delta: &Vector3<f32>) {
+        let new_params = self.model.clone().into_vec() + delta;
+        let new_params = new_params.xy().normalize().push(new_params.z);
+        self.model = Line::from_vec(new_params);
+    }
+
+    fn residuals(&self) -> Matrix<f32, U2, Dynamic, Self::ResidualStorage> {
+        let residual_data = self
+            .points
+            .iter()
+            .flat_map(|&point| {
+                use std::iter::once;
+                let vec = self.model.xy_residuals(point);
+                once(vec.x).chain(once(vec.y))
+            })
+            .collect();
+        RESIDUAL_SCALE * Matrix::<f32, U2, Dynamic, Self::ResidualStorage>::from_vec(residual_data)
+    }
+
+    fn transposed_jacobian(&self, i: usize) -> Matrix3x2<f32> {
+        self.model.jacobian(self.points[i])
     }
 }
 
@@ -90,9 +130,9 @@ fn lines() {
 
     for _ in 0..LINES_TO_ESTIMATE {
         // Generate <a, b> and normalize.
-        let norm = Vector2::new(rng.gen_range(-10.0, 10.0), rng.gen_range(-10.0, 10.0)).normalize();
+        let normal = Vector2::new(rng.gen_range(-10.0, 10.0), rng.gen_range(-10.0, 10.0)).normalize();
         // Get parallel ray.
-        let ray = Vector2::new(norm.y, -norm.x);
+        let ray = Vector2::new(normal.y, -normal.x);
         // Generate random c.
         let c = rng.gen_range(-10.0, 10.0);
 
@@ -108,7 +148,7 @@ fn lines() {
                 let residual: f32 = residuals.sample(&mut rng);
                 let distance: f32 = distances.sample(&mut rng);
                 let along = ray * distance;
-                let against = (residual - c) * norm;
+                let against = (residual - c) * normal;
                 along + against
             })
             .collect();
@@ -116,37 +156,15 @@ fn lines() {
         let model = arrsac
             .model(&LineEstimator, points.iter().copied())
             .expect("unable to estimate a model");
-
-        let real_model = Line { norm, c };
-
         // Now perform Levenberg-Marquardt.
-        let model = Line::from_vec(levenberg_marquardt::optimize(
-            levenberg_marquardt::Config::default(),
-            model.into_vec(),
-            |v| v.xy().normalize().push(v.z),
-            |v| {
-                let model = Line::from_vec(*v);
-
-                let residual_data = points
-                    .iter()
-                    .flat_map(|&point| {
-                        use std::iter::once;
-                        let vec = model.xy_residuals(point);
-                        once(vec.x).chain(once(vec.y))
-                    })
-                    .collect();
-                RESIDUAL_SCALE
-                    * Matrix::<f32, U2, Dynamic, VecStorage<f32, U2, Dynamic>>::from_vec(
-                        residual_data,
-                    )
-            },
-            |v| {
-                let model = Line::from_vec(*v);
-
-                points.iter().map(move |&point| model.jacobian(point))
-            },
-        ));
-
+        let model = LevenbergMarquardt::default()
+            .minimize(LineFittingOptimizationProblem {
+                // initial value
+                model,
+                points: &points,
+            })
+            .model;
+        let real_model = Line { normal, c };
         let new_cosine_distance = model.norm_cosine_distance(&real_model);
 
         // Check the slope using the cosine distance.
